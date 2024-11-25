a22809f2049db58ee0736df759e3c93eda964085
WentChung <wentcastillo@gmail.com>
2024-11-24 23:28:48 -0500

    Update footer.php

--- views/footer.php
+++ views/footer.php
@@ -3,21 +3,26 @@
     <div class="footer-grid">
       <div class="footer-section">
         <h3>Contactos</h3>
-        <p>Teléfono 1: 6222-2222</p>
-        <p>Teléfono 2: 6444-4444</p>
+        <p>Teléfono #1: 6222-2222</p>
+        <p>Teléfono #2: 6444-4444</p>
         <h4>Email</h4>
-        <p>Email 1: info@fitLife.com</p>
-        <p>Email 2: soporte@fitLife.com</p>
+        <p>Email #1: info@fitLife.com</p>
+        <p>Email #2: soporte@fitLife.com</p>
       </div>
       <div class="footer-section">
         <h3>Legal</h3>
         <p><a href="terminos.php">Términos y Condiciones</a></p> 
+        <p>El contenido de esta página web/aplicación es de carácter informativo y no sustituye el asesoramiento médico o profesional. 
+          Antes de iniciar cualquier programa de ejercicio o dieta, consulte con su médico o especialista. Los resultados pueden variar 
+          según cada persona. No nos hacemos responsables por lesiones, daños o inconvenientes derivados del uso de la información o servicios ofrecidos.</p> 
       </div>
       <div class="footer-section">
         <h3>Redes Sociales</h3>
         <ul class="social-links">
-          <li><a href="#"><img src="./src/images/facebook-svgrepo-com.svg" alt="Facebook"></a></li>
-          <li><a href="#"><img src="./src/images/instagram-svgrepo-com.svg" alt="Instagram"></a></li>
+          <li><a href="https://www.facebook.com"><img src="./src/images/facebook.png" alt="Facebook"></a></li>
+          <li><a href="https://www.instagram.com"><img src="./src/images/instagram.png" alt="Instagram"></a></li>
+          <li><a href="https://x.com"><img src="./src/images/twitter.png" alt="Twitter"></a></li>
+          <li><a href="https://linkedin.com"><img src="./src/images/linkedin.png" alt="Linkedin"></a></li>
         </ul>
       </div>
     </div>
